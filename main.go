6f3eef6900a9404e5b6d2ad8e39f0f8d78f5de64
Ayushman Gupta <guptaayushman24@gmail.com>
2025-02-21 18:07:18 +0530

    main.go file has been changed

--- main.go
+++ main.go
@@ -4,35 +4,9 @@ import (
 	database "ZOCKET/config"
 	"ZOCKET/routes"
 
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/websocket/v2"
 )
 
-var clients map[string]*websocket.Conn
-
-// WebSocket endpoint to register clients
-func wsHandler(c *websocket.Conn) {
-	// For this example, assume the email is sent as the first message.
-	_, msg, err := c.ReadMessage()
-	if err != nil {
-		c.Close()
-		return
-	}
-	userEmail := string(msg)
-	clients[userEmail] = c
-	fmt.Printf("User %s connected\n", userEmail)
-
-	// Keep connection open
-	for {
-		if _, _, err := c.ReadMessage(); err != nil {
-			delete(clients, userEmail)
-			c.Close()
-			break
-		}
-	}
-}
 func main() {
 	// Connent to db
 	database.ConnectDB()
@@ -45,8 +19,6 @@ func main() {
 
 	collection := database.GetCollection("users")
 	database.EnsureUniqueIndex(collection)
-	// WebSocket route
-	r.Get("/ws", websocket.New(wsHandler))
 
 	// Start the server
 	r.Listen(":8000")
